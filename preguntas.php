3fccc5d32e25dffcdf4517ef7e5de317550f7fa5
Manuel Torres Gil <mtorres@ual.es>
2016-10-18 10:51:24 +0200

    Supresión de edX

--- preguntas.php
+++ preguntas.php
@@ -8,7 +8,7 @@ $preguntas[1] = array(
 										'respuesta' => 'Ofrece Infraestructura como servicio (IaaS) mediante <i>OpenStack</i> - con el puedes crear máquinas virtuales en la nube -, '.
 																	' alojamiento de archivos (similar a <i>Dropbox</i>), Sistemas de control de versiones ' .
 																	'(<i>Git</i> y <i>Subversion</i>), Gestión de proyectos (<i>Redmine</i>), y plataformas de docencia on-line ' .
-																	'(<i>Moodle</i> y <i>edX</i>)'),
+																	'(<i>Moodle</i>'),
 									2 => array (
 										'pregunta' => '¿Qué personas pueden tener acceso a estos servicios?', 
 										'respuesta' => 'Miembros del Departamento de Informática, becarios o contratados de investigación asociados al ' .
@@ -28,7 +28,7 @@ $preguntas[1] = array(
 																	'en http://redmine.di.ual.es solicitando el nuevo servicio y el procedimiento para configurarlo'),
 									6 => array (
 										'pregunta' => '¿Cómo puedo aprender a usar los servicios de Cloud-DI?', 
-										'respuesta' => 'En <a href = "http://edx.di.ual.es">http://edx.di.ual.es</a> encontrarás cursos on-line que te '.
+										'respuesta' => 'En <a href = "http://moodle.di.ual.es">http://moodle.di.ual.es</a> encontrarás cursos on-line que te '.
 																	'ayudarán a formarte en los servicios de Cloud-DI')
 									)
 							);
@@ -53,8 +53,7 @@ $preguntas[2] = array(
 										'respuesta' => 'En <a href = "http://ldap.di.ual.es">http://ldap.di.ual.es</a> puedes cambiar tu contraseña'),
 									5 => array (
 										'pregunta' => 'He perdido mi contraseña', 
-										'respuesta' => 'Envía un email a <a href="mailto:diosdado@ual.es">diosdado@ual.es</a>, <a href="mailto:isalinas@ual.es">isalinas@ual.es</a> ' .
-																	'o <a href="mailto:jmembriv@ual.es">jmembriv@ual.es</a> solicitando que restablezcan tu contraseña'),
+										'respuesta' => 'Envía un email a <a href="mailto:diosdado@ual.es">diosdado@ual.es</a> solicitando que restablezcan tu contraseña'),
 									6 => array (
 										'pregunta' => '¿Caduca la contraseña?', 
 										'respuesta' => 'De forma predeterminada no se exige cambiar la contraseña periódicamente, aunque por motivos ' .
@@ -87,8 +86,7 @@ $preguntas[3] = array(
 										'respuesta' => 'En los ajustes de tu cuenta OpenStack-DI'),
 									5 => array (
 										'pregunta' => 'He perdido mi contraseña', 
-										'respuesta' => 'Envía un email a <a href="mailto:diosdado@ual.es">diosdado@ual.es</a>, <a href="mailto:isalinas@ual.es">isalinas@ual.es</a> ' .
-																	'o <a href="mailto:jmembriv@ual.es">jmembriv@ual.es</a> solicitando que restablezcan tu contraseña'),									
+										'respuesta' => 'Envía un email a <a href="mailto:diosdado@ual.es">diosdado@ual.es</a> solicitando que restablezcan tu contraseña'),									
 									6 => array (
 										'pregunta' => '¿Caduca la contraseña?', 
 										'respuesta' => 'De forma predeterminada no se exige cambiar la contraseña periódicamente, aunque por motivos ' .
@@ -103,7 +101,7 @@ $preguntas[3] = array(
 																	'apellidos, nombre, login, email'),
 									9 => array (
 										'pregunta' => '¿Cómo puedo aprender a utilizar los servicios básicos de OpenStack-DI?', 
-										'respuesta' => 'En <a href = "http://edx.di.ual.es">http://edx.di.ual.es</a> encontrarás cursos on-line que te '.
+										'respuesta' => 'En <a href = "http://moodle.di.ual.es">http://moodle.di.ual.es</a> encontrarás cursos on-line que te '.
 																	'ayudarán a formarte en OpenStack-DI'),		
 									10 => array (
 										'pregunta' => '¿Cuál es mi cuota en OpenStack-DI?', 
@@ -121,19 +119,19 @@ $preguntas[3] = array(
 																	'conectarnos a ella desde cualquier parte de la UAL, o desde fuera mediante VPN.<br/>' .
 																	'Si quieres que tu máquina virtual sea accesible desde la red de UAL o desde fuera mediante VPN, ' .
 																	'tendrás que asociarle una IP flotante.<br/>' .
-																	'Para más información, consulta en <a href = "http://edx.di.ual.es">http://edx.di.ual.es</a> el curso '.
+																	'Para más información, consulta en <a href = "http://moodle.di.ual.es">http://moodle.di.ual.es</a> el curso '.
 																	'on-line de Introducción al uso de OpenStack'),		
 									12 => array (
 										'pregunta' => '¿Cómo puedo crear una instancia Windows?', 
 										'respuesta' => 'Lanza una instancia seleccionando una imagen o snapshot disponible de Windows. Configura el puerto RDP (3389) para que ' .
 																	'puedas acceder por escritorio remoto a ella.<br/>' .
-																	'Para más información, consulta en <a href = "http://edx.di.ual.es">http://edx.di.ual.es</a> el curso '.
+																	'Para más información, consulta en <a href = "http://moodle.di.ual.es">http://moodle.di.ual.es</a> el curso '.
 																	'on-line de Introducción al uso de OpenStack'),		
 									13 => array (
 										'pregunta' => '¿Cómo puedo crear una instancia Linux?', 
 										'respuesta' => 'Asegúrate de haber subido a OpenStack-DI tu clave pública. A continuación, lanza una instancia seleccionando una imagen ' .
 																	'o snapshot disponible de Linux. Configura el puerto SSH (22) para que puedas acceder a ella.<br/>' .
-																	'Para más información, consulta en <a href = "http://edx.di.ual.es">http://edx.di.ual.es</a> el curso '.
+																	'Para más información, consulta en <a href = "http://moodle.di.ual.es">http://moodle.di.ual.es</a> el curso '.
 																	'on-line de Introducción al uso de OpenStack'),		
 									14 => array (
 										'pregunta' => 'No puedo conectarme a una instancia de Windows', 
@@ -179,11 +177,11 @@ $preguntas[4] = array(
 										'pregunta' => '¿Existe un cliente para mi ordenador, tablet o smartphone?', 
 										'respuesta' => '<a href = "https://owncloud.org/install/#install-clients">Descarga tu cliente</a> para tu ordenador ' .
 																	'Mac, Windows o Linux o para tu dispositivo táctil Android o iOS.</br>' .
-																	'En <a href = "http://edx.di.ual.es">http://edx.di.ual.es</a> encontrarás el curso on-line sobre servicios ' .
+																	'En <a href = "http://moodle.di.ual.es">http://moodle.di.ual.es</a> encontrarás el curso on-line sobre servicios ' .
 																	'de Cloud-DI, donde hay una sección dedicada a instalación de clientes ownCloud'),
 									6 => array (
 										'pregunta' => '¿Puedo usar mi cuenta ownCloud-DI como una unidad WebDAV?', 
-										'respuesta' => 'Sí, en <a href = "http://edx.di.ual.es">http://edx.di.ual.es</a> encontrarás el curso on-line sobre servicios ' .
+										'respuesta' => 'Sí, en <a href = "http://moodle.di.ual.es">http://moodle.di.ual.es</a> encontrarás el curso on-line sobre servicios ' .
 																	'de Cloud-DI, donde hay una sección dedicada la configuración de WebDAV')
 									)
 							);
@@ -211,6 +209,22 @@ $preguntas[5] = array(
 							);
 
 $preguntas[6] = array(
+							'categoria' => 'Moodle-DI',
+							'preguntas' => array(
+									1 => array (
+										'pregunta' => '¿Qué servicio ofrece Moodle-DI?', 
+										'respuesta' => 'Soporte para docencia online'),
+									2 => array (
+										'pregunta' => '¿Qué personas pueden tener acceso a Moodle-DI?', 
+										'respuesta' => 'Todas las personas que estén registradas en LDAP-DI'),
+									3 => array (
+										'pregunta' => '¿Cuáles son mis credenciales de acceso a Moodle-DI?', 
+										'respuesta' => 'Las credenciales que tienes almacenadas en LDAP-DI')
+									)
+							);
+
+/*
+$preguntas[6] = array(
 							'categoria' => 'edX-DI',
 							'preguntas' => array(
 									1 => array (
@@ -228,5 +242,6 @@ $preguntas[6] = array(
 										'respuesta' => 'edX-DI tiene su propio sistema de autentificación, independiente de LDAP-DI')
 									)
 							);
+*/
 
 ?>
